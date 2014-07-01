efe400f62ed694aa3ab791763f437e471dbc3f30
Brandon Haugen <bhaugen@datalogics.com>
2014-07-01 17:53:23 -0500

    DecorateDocumentRequest : start of decorate task
    Will invoke the DecorateDocument API eventually

---
+++ DatalogicsPDFWebAPILibrary/PDFWebAPILibrary/src/main/java/com/datalogics/pdf/web/DecorateDocumentRequest.java
@@ -0,0 +1,16 @@
+package com.datalogics.pdf.web;
+
+/**
+ * Created by bhaugen on 7/1/14.
+ */
+public class DecorateDocumentRequest extends BasicRequest {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        // TODO any special processing that should happen for the DecorateDocumentRequest
+
+        // Then call BasicRequest.doInBackground
+        super.doInBackground(objects);
+        return null;
+    }
+}
