9e39c03c292d82a66abbc43ada1e0f9b8584d0c2
Brandon Haugen <bhaugen@datalogics.com>
2014-07-03 15:06:12 -0500

    DecorateDocumentRequest : add first impl
    This creates the required form parts and passes
    them down to the BasicRequest class.
    
    When using this request currently though a 500
    error from the server is returned.

--- DatalogicsPDFWebAPILibrary/PDFWebAPILibrary/src/main/java/com/datalogics/pdf/web/DecorateDocumentRequest.java
+++ DatalogicsPDFWebAPILibrary/PDFWebAPILibrary/src/main/java/com/datalogics/pdf/web/DecorateDocumentRequest.java
@@ -1,5 +1,9 @@
 package com.datalogics.pdf.web;
 
+import com.squareup.mimecraft.Part;
+
+import java.io.File;
+
 /**
  * Created by bhaugen on 7/1/14.
  */
@@ -16,10 +20,18 @@ public class DecorateDocumentRequest extends BasicRequest {
 
     @Override
     protected Object doInBackground(Object[] objects) {
-        // TODO any special processing that should happen for the DecorateDocumentRequest
+        // build the parts of the multipart form that are specific to the decorate document request
+
+        // first is the inputPart that contains the input file (the PDF) to decorate
+        Part inputPart = new Part.Builder().body(new File(inputFile)).contentDisposition("form-data; name=\"input\"").contentType("application/pdf").build();
+
+        // second is the decorationPart that contains the xml file to use to decorate the PDF
+        Part decorationPart = new Part.Builder().body(new File(decorationDataFile)).contentDisposition("form-data; name=\"decorationData\"").contentType("text/xml").build();
+
+        // create a new objects array to pass the info we need to down to the BasicRequest class
+        objects = new Object[] {BasicRequest.WEB_API_URL + REQUEST_PATH, inputPart, decorationPart};
 
         // Then call BasicRequest.doInBackground
-        super.doInBackground(objects);
-        return null;
+        return super.doInBackground(objects);
     }
 }
